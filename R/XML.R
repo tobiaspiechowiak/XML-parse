f04ed1101f162524f00863a008b8ecd386b4e670
Tobias Piechowiak <tpiechowiak@gnresound.com>
2018-06-04 17:25:10 +0200

    Different implementation

--- R/XML.R
+++ R/XML.R
@@ -16,12 +16,12 @@ f <- seq(0,10000,43)
 #REUG
 REUG <- xmltop[['Session']][[7]][['Data']][['RecordedData']][['Measured']]
 
-for (i in length(f) ){
-  REUG.value[i] <- xmlValue(REUG[['XYData']][['Y']])
-  
+for (i in 0:(length(f) - 2)){
+  tmp[i] <- as.numeric(xmlValue(REUG[[17 + i]][['Y']]))
 }
 
 
 #REAG
 REAG_65dB <- xmltop[['Session']][[7]][['Data']][['RecordedData']][['Measured']]
 
+#xmlValue(xml[[1]][[1]][[2]][[7]][[3]][[1]][[2]][[19]][[2]])
